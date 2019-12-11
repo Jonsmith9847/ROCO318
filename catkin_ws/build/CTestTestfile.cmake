bebf226b593073f655c84f4d664db5c05fced816
JonSmith9847 <jonathan.smith@students.plymouth.ac.uk>
2019-12-11 00:58:59 +0000

    Added catkin workspace and usb_cam package

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jon/ROCO318/catkin_ws/src
+# Build directory: /home/jon/ROCO318/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(usb_cam)
