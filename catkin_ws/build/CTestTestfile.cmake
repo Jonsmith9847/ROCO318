2f605567518a011deab06c529ab116662ca2f8fd
JonSmith9847 <jonathan.smith@students.plymouth.ac.uk>
2020-01-05 23:39:50 +0000

    Added controller manager

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,8 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
-subdirs(tilt_urdf)
-subdirs(motor_publisher)
-subdirs(joint_state_publisher/joint_state_publisher)
-subdirs(motor_controller)
+subdirs(dynamixel_motors)
 subdirs(usb_cam)
---
+++ catkin_ws/build/dynamixel_motors/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jon/ROCO318/catkin_ws/src/dynamixel_motors
+# Build directory: /home/jon/ROCO318/catkin_ws/build/dynamixel_motors
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
